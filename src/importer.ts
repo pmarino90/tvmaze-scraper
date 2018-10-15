3452d88454ab0a3058f152079157c24ae86a39ba
Paolo Marino <paolomarino@reaktor.fi>
2018-10-16 00:02:57 +0200

    Importer fatches cast members as well

--- src/importer.ts
+++ src/importer.ts
@@ -28,55 +28,19 @@ interface Status {
 
 type FetchError = "unknownError" | "retryError";
 
-const policy = capDelay(
+const initialStatus: Status = {
+  currentPage: 1,
+  items: [],
+  error: none
+};
+
+const retryPolicy = capDelay(
   RETRY_DELAY,
   monoidRetryPolicy.concat(
     exponentialBackoff(RETRY_BACKOFF),
     limitRetries(RETRY_TIMES)
   )
 );
-
-// However this must have proper validation to ensure type is correct
-const parseShowsResponse = (items: TVMazeShow[]): Show[] =>
-  items.map(({ id, name }) => ({ id, name, cast: [] }));
-
-// However this must have proper validation to ensure type is correct
-const parseCastMembersResponse = (items: TVMazeCastMember[]): CastMember[] =>
-  items.map(({ person }) => ({
-    id: person.id,
-    name: person.name,
-    birthday: person.birthday
-  }));
-
-const fetchShowPage = (page: number) => {
-  return retrying(
-    policy,
-    () =>
-      get<TVMazeShow[]>(`http://api.tvmaze.com/shows?page=${page}`).map(
-        parseShowsResponse
-      ),
-    e =>
-      e.fold(
-        (error: ClientError) => error.type === "RateLimitError",
-        () => false
-      )
-  );
-};
-
-const fetchShowCastMembers = (show: Show) => {
-  return retrying(
-    policy,
-    () =>
-      get<TVMazeCastMember[]>(
-        `http://api.tvmaze.com/shows/${show.id}/cast`
-      ).map(parseCastMembersResponse),
-    e =>
-      e.fold(
-        (error: ClientError) => error.type === "RateLimitError",
-        () => false
-      )
-  );
-};
 
 const foldApiError = <R>(
   error: ClientError,
@@ -94,16 +58,57 @@ const foldApiError = <R>(
   }
 };
 
-const initialStatus: Status = {
-  currentPage: 1,
-  items: [],
-  error: none
+// However this must have proper validation to ensure type is correct
+const parseShowsResponse = (items: TVMazeShow[]): Show[] =>
+  items.map(({ id, name }) => ({ id, name, cast: [] }));
+
+// However this must have proper validation to ensure type is correct
+const parseCastMembersResponse = (items: TVMazeCastMember[]): CastMember[] =>
+  items.map(({ person }) => ({
+    id: person.id,
+    name: person.name,
+    birthday: person.birthday
+  }));
+
+const fetchShowPage = (page: number) => {
+  return retrying(
+    retryPolicy,
+    () =>
+      get<TVMazeShow[]>(`http://api.tvmaze.com/shows?page=${page}`).map(
+        parseShowsResponse
+      ),
+    e =>
+      e.fold(
+        (error: ClientError) => error.type === "RateLimitError",
+        () => false
+      )
+  );
+};
+
+const fetchShowCastMembers = (show: Show) => {
+  return retrying(
+    retryPolicy,
+    () =>
+      get<TVMazeCastMember[]>(
+        `http://api.tvmaze.com/shows/${show.id}/cast`
+      ).map(parseCastMembersResponse),
+    e =>
+      e.fold(
+        (error: ClientError) => error.type === "RateLimitError",
+        () => false
+      )
+  );
 };
 
 const collectShows = (status: Status, items: Show[]): Status => ({
   currentPage: status.currentPage + 1,
-  items: status.items.concat(items),
+  items,
   error: none
+});
+
+const hydrateShows = (status: Status, items: Show[]): Status => ({
+  ...status,
+  items
 });
 
 const setError = (status: Status, error: Option<ClientError>): Status => ({
@@ -114,16 +119,30 @@ const setError = (status: Status, error: Option<ClientError>): Status => ({
 const setCast = (show: Show, cast: CastMember[]): Show => ({ ...show, cast });
 
 function scrapeShowsApi(): Task<[Show[], Option<FetchError>]> {
-  const runStatus = (status: Status): Task<Status> => {
+  const runPageRetreival = (status: Status): Task<Status> => {
+    console.log(`Requesting page ${status.currentPage}...`);
+
     return fetchShowPage(status.currentPage).fold(
       error => setError(status, some(error)),
-      items => collectShows(status, items)
+      items => hydrateShows(status, items)
     );
+  };
+
+  const runCastRetrieval = (status: Status): Task<Status> => {
+    console.log(`Requesting cast members..`);
+    return sequence(task, array)(
+      status.items.map(show =>
+        fetchShowCastMembers(show).fold(() => show, cast => setCast(show, cast))
+      )
+    ).map(shows => collectShows(status, shows));
   };
 
   const run = (status: Status): Task<[Show[], Option<FetchError>]> => {
     return status.error.foldL(
-      () => runStatus(status).chain(run),
+      () =>
+        runPageRetreival(status)
+          .chain(runCastRetrieval)
+          .chain(run),
       error =>
         task.of(
           tuple(
@@ -141,3 +160,7 @@ function scrapeShowsApi(): Task<[Show[], Option<FetchError>]> {
 
   return run(initialStatus);
 }
+
+scrapeShowsApi()
+  .run()
+  .then(data => console.log(data));
